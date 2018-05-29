eed28ff1b5c67fca35a0ddacde6fc5087db5ed9a
Ungureanu Adrian <ady8_8@yahoo.com>
2018-05-29 14:39:01 +0300

    added sort with step description for insertion sort

--- src/main/java/com/adrian/services/InsertionSortService.java
+++ src/main/java/com/adrian/services/InsertionSortService.java
@@ -5,8 +5,8 @@ import java.util.function.BiPredicate;
 
 import com.adrian.enums.SortAlgorithm;
 import com.adrian.qualifiers.InsertionSort;
-import com.adrian.services.responses.SortResponseWithSteps;
 import com.adrian.services.responses.Step;
+import com.adrian.services.responses.StepItem;
 import com.adrian.utils.AlgorithmsUtil;
 
 @InsertionSort
@@ -14,13 +14,27 @@ public class InsertionSortService<T extends Comparable<T>> extends AbstractSortS
 
 	@Override
 	protected void actualSortWithSteps(T[] array, BiPredicate<T, T> predicate, List<Step> steps) {
-		// TODO Auto-generated method stub
+		int length = array.length;
 
-	}
+		for (int i = 1; i < length; i++) {
+			final Step insertItemStep = new Step("Insert item step.", i,
+					String.format("Insert item at position %d in the sorted array"));
+			int numberExchanges = 0;
+			for (int j = i; j > 0 && predicate.test(array[j], array[j - 1]); j--) {
+				AlgorithmsUtil.exchange(array, j - 1, j);
+				numberExchanges++;
+			}
+			StepItem sortedStepItem = new StepItem("Number of items already sorted", Integer.toString(i));
+			StepItem exchangesMadeStepItem = new StepItem("Number of exchanges made",
+					Integer.toString(numberExchanges));
+			StepItem insertionIndexStepItem = new StepItem("Index where the insertion should be made",
+					Integer.toString(i - numberExchanges));
 
-	@Override
-	public SortResponseWithSteps<T> sortWithSteps(T[] array, BiPredicate<T, T> predicate) {
-		return null;
+			insertItemStep
+					.setStepItems(new StepItem[] { sortedStepItem, exchangesMadeStepItem, insertionIndexStepItem });
+			steps.add(insertItemStep);
+		}
+
 	}
 
 	@Override
--- src/main/java/com/adrian/services/ShellSortService.java
+++ src/main/java/com/adrian/services/ShellSortService.java
@@ -13,7 +13,23 @@ public class ShellSortService<T extends Comparable<T>> extends AbstractSortServi
 
 	@Override
 	protected void actualSortWithSteps(T[] array, BiPredicate<T, T> predicate, List<Step> steps) {
-		// TODO Auto-generated method stub
+		int length = array.length;
+
+		int step = 1;
+
+		while (step < length / 3) {
+			step = step * 3 + 1;
+		}
+
+		while (step >= 1) {
+			for (int i = step; i < length; i++) {
+				for (int j = i; j >= step && predicate.test(array[j], array[j - step]); j -= step) {
+					AlgorithmsUtil.exchange(array, j - step, j);
+				}
+			}
+
+			step = step / 3;
+		}
 
 	}
 
